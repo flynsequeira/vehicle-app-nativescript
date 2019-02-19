44afa15f119a873a7cdcab0202140aad511a824f
Flyn Sequeira <flyns3694@gmail.com>
2019-02-19 20:43:53 +0530

    Fixed config url

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,5 +1,6 @@
 export class Config {
-  static apiUrl = "http://ec2-18-191-241-71.us-east-2.compute.amazonaws.com:3000/";
+  static apiUrl = "http://ec2-18-191-248-219.us-east-2.compute.amazonaws.com:3000/";
+  // static apiUrl = "http://localhost:3000/";
   static token = "";
   static user = {};
 }
